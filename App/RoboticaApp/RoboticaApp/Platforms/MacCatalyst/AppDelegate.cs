348e49d7e98e4280f9a6fec0ec7e013f1ecd78dc
kajatrijsburg <kajatrijsburg@gmail.com>
2023-04-19 16:33:57 +0200

    Moved the app and app unit tests to be part of the main solution

--- App/RoboticaApp/RoboticaApp/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace RoboticaApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
