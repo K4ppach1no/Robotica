1903c1d01ae43d4288ef2dc1c13aac8aa35175f1
kajatrijsburg <kajatrijsburg@gmail.com>
2023-04-19 12:51:01 +0200

    initial commit for the app

---
+++ App/RoboticaApp/RoboticaApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RoboticaApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
