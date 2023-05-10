05105f15a404145390530351d2067fc297013981
Bard <nigel.terpstra@student.nhlstenden.com>
2023-05-10 11:39:35 +0200

    added cmake option, improved the design of the shell script

--- install-packages.sh
+++ install-packages.sh
@@ -5,76 +5,131 @@ ENV_VARIABLES=$(powershell -c "[Environment]::GetEnvironmentVariable('PATH', 'Ma
 DEFAULT_CHOCOLATEY_PATH="C:/ProgramData/Chocolatey"
 PACKAGES_DIR="$PROGRAM_FILES_PATH/packages/"
 
-function add_package_exe(){
-	local tar_url=$1
-	local name=$2
-	
-	local LOCAL_PACKAGE_DIR="$PACKAGES_DIR"
-	
-	if [ -d "$LOCAL_PACKAGE_DIR$name" ]; then
-		echo "$LOCAL_PACKAGE_DIR already exists"
-		return
-	else
-		mkdir "$PACKAGES_DIR"
-	fi
-	cd "$LOCAL_PACKAGE_DIR"
-	wget "$tar_url" -O "$name.exe"
-	powershell -WindowStyle Hidden "start cmd \"/k $name.exe -o . -y -gm2\" -v runAs"
-	#rm "$name.exe"
+# install chocolatey
+function install_chocolatey() {
+
+  # check if chocolatey is already installed
+  if ! command -v choco &>/dev/null; then
+    # install chocolatey
+    powershell.exe -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+  else
+    echo "chocolatey is already installed"
+  fi
+}
+
+function install_using_choco() {
+  for package_name in "$@"; do
+    # check if $package_name is already installed
+    if command -v $package_name &>/dev/null; then
+      echo "$package_name is already installed"
+      continue
+    fi
+
+    # install $package_name using chocolatey
+    powershell.exe -ExecutionPolicy Bypass -Command "choco install $package_name -y"
+
+    #get exe path in chocolatey
+    EXE_PATH_CHOCOLATEY=$(powershell.exe -ExecutionPolicy Bypass -Command "(Get-ChildItem -Path $env:ChocolateyInstall -Filter \"$package_name.exe\" -Recurse | Select-Object -First 1).DirectoryName")
+
+    #exe path to program files, choco doesn't always install it to the choco bin
+    EXE_PATH_PROGRAMFILES=$(powershell.exe -ExecutionPolicy Bypass -Command "(Get-ChildItem -Path \"$PROGRAM_FILES_PATH\" -Filter \"$package_name.exe\" -Recurse | Select-Object -First 1).DirectoryName")
+
+    #if we can find package.exe in chocolatey files add it to the env variables
+    if [[ -n "$EXE_PATH_CHOCOLATEY" ]]; then
+      ENV_VARIABLES="$EXE_PATH_CHOCOLATEY;$ENV_VARIABLES"
+      export PATH="$(echo $EXE_PATH_CHOCOLATEY | sed 's/^\([a-zA-Z]\):/\/\1/'):$PATH"
+
+    #else we look in the program files directory, if it's installed there we add it
+    elif [[ -n "$EXE_PATH_PROGRAMFILES" ]]; then
+      ENV_VARIABLES="$EXE_PATH_PROGRAMFILES;$ENV_VARIABLES"
+      export PATH="$(echo $EXE_PATH_PROGRAMFILES | sed 's/^\([a-zA-Z]\):/\/\1/'):$PATH"
+
+    #else give error
+    else
+      echo "can't find install directory"
+    fi
+  done
+  #add env variables permanently
+  powershell.exe -Command "[Environment]::SetEnvironmentVariable('PATH', '"$ENV_VARIABLES"', 'Machine')"
+}
+
+function add_package_exe() {
+  local tar_url=$1
+  local name=$2
+
+  local LOCAL_PACKAGE_DIR="$PACKAGES_DIR$name"
+
+  #check if package already exists
+  if [[ -d "$LOCAL_PACKAGE_DIR" ]]; then
+    echo "$name already exists"
+    return
+  fi
+  
+  # make directory for package
+  mkdir -p "$LOCAL_PACKAGE_DIR"
+
+  cd "$LOCAL_PACKAGE_DIR" || die
+  wget "$tar_url" -O "$name.exe"
+  
+  chmod +x "$name".exe
+  ./"$name".exe -o . -y -gm2
+  rm "$name.exe"
+}
+
+function install_from_github_using_cmake() {
+  local url=$1
+  local name=$2
+  local LOCAL_PACKAGE_DIR="$PACKAGES_DIR$name"
+
+  #check if package already exists
+  if [[ -d "$LOCAL_PACKAGE_DIR" ]]; then
+    echo "$name already exists"
+    return
+  fi
+
+  # download the package from gitHub
+  curl -L -o "$name.zip" "$url"
+
+  # make directory for package
+  mkdir -p "$LOCAL_PACKAGE_DIR"
+
+  # unzip the file
+  unzip -d "$LOCAL_PACKAGE_DIR" "$name.zip"
+
+  # remove the downloaded zip file
+  rm "$name.zip"
+
+  cd "$LOCAL_PACKAGE_DIR" || exit
+
+  # if the zip has only one directory and 0 files in it go into that
+  if [ "$(find . -maxdepth 1 -type d | wc -l)" -eq 2 ] && [ "$(find . -maxdepth 1 -type f | wc -l)" -eq 0 ]; then
+    cd "$(find . -maxdepth 1 -type d -not -path "." -print)" || exit
+  fi
+
+  # release
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+  cmake --build build --config Release
+  cmake --install build --config Release --prefix "$LOCAL_PACKAGE_DIR/release"
+
+  # debugs
+  cmake -S . -B build-debug -DCMAKE_BUILD_TYPE=Debug
+  cmake --build build-debug --config Debug
+  cmake --install build-debug --config Debug --prefix "$LOCAL_PACKAGE_DIR/debug"
 }
 
 # check if cmd is running as admin
 if ! net session >nul 2>&1; then
-	echo "not admin"
-	powershell -WindowStyle Hidden "start cmd \"/k cd /d $MAIN_PATH\\ && $0\" -v runAs"
-	exit 0;
+  echo "not admin"
+  powershell -WindowStyle Hidden "start cmd \"/k cd /d $MAIN_PATH\\ && $0 && exit\" -WindowStyle Hidden -Verb runAs"
+  exit
 fi
 
-# install chocolatey
-if ! command -v choco > /dev/null 2>&1 && [[ ! -d "$DEFAULT_CHOCOLATEY_PATH" ]]; then
-	echo "installing chocolatey"
-	#install chocolatey
-	powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
-	#add libs to env variables
-	ENV_VARIABLES="$(powershell -c '${env:ChocolateyInstall}')/bin/lib*;$ENV_VARIABLES"
-	echo "chocolatey installed"
-else
-	echo "chocolatey already installed"
-fi
-# Dependencies
-declare -A dependencies=(
-  #[Git]=2.40.0 #- probably already installed
-  [wget]=1.21.1
-  [CMake]=3.26.3
-  [Tar]=1.34
-  [make]=4.3
-  #[7zip]=19.0
-)
+install_chocolatey
+install_using_choco "unzip" "cmake" "tar" "wget"
 
-# Check if each dependency is installed and install missing ones
-for dep in "${!dependencies[@]}"; do
-	version=${dependencies[$dep]}
-	#if dependency has already been installed
-	if command -v "$dep" > /dev/null 2>&1; then
-		installed_version=`$dep --version | grep -Eo '[0-9]+(\.[0-9]+)*'`
-		#if the installed version is different than specified version
-		#and if it has been installed by choco, we don't want to double install stuff
-		if [[ "$installed_version" != "$version" ]] && [ -n "$(powershell -command "choco list -l | findstr -I $dep")" ]; then
-			echo "upgrading $dep to $version"
-			choco upgrade "$dep" --version="$version" -y
-		fi
-	else
-		echo "installing $dep"
-		choco install "$dep" --version="$version" -y -ia "/INSTALLDIR:$PROGRAM_FILES_PATH/$dep"
-		
-		#set path to env variables if it's not already set
-		if  [[ -d "$PROGRAM_FILES_PATH/$dep/bin" ]]; then
-			ENV_VARIABLES="$PROGRAM_FILES_PATH/$dep/bin;$ENV_VARIABLES"
-			powershell -Command "[Environment]::SetEnvironmentVariable('PATH', '"$ENV_VARIABLES"', 'Machine')"
-			echo "$dep installed and added to env variables"
-		fi
-	fi
-done
 add_package_exe "https://github.com/opencv/opencv/releases/download/4.7.0/opencv-4.7.0-windows.exe" "opencv"
 
+install_from_github_using_cmake "https://github.com/martinus/nanobench/archive/refs/tags/v4.3.11.zip" "nanobench"
+
+rm nul
 echo "All done"
