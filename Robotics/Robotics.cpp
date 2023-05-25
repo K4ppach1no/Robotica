62db361b797eeb5787b725570af7f6df80e22e2f
Alissa de Vegt <alissa.de.vegt@student.nhlstenden.com>
2023-05-25 13:10:44 +0200

    live feed

--- Robotics/Robotics.cpp
+++ Robotics/Robotics.cpp
@@ -16,8 +16,44 @@ Mat blur_difference(Mat img, int h1, int s1, int h2, int s2)
     return dif;
 }
 
+
 int main()
 {
+    Mat frame, gray, boxed;
+    VideoCapture cap = VideoCapture(0);
+    cap.read(frame);
+
+    for (;;)
+    {
+        // wait for a new frame from camera and store it into 'frame'
+        cap.read(frame);
+        // check if we succeeded
+        if (frame.empty()) {
+            std::cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+
+
+        Mat grey, thr;
+        std::vector<std::vector<Point> > contours;
+        std::vector<Vec4i> hierarchy;
+        cv::resize(frame, frame, { 500, 500 }, 0, 0, cv::INTER_NEAREST);
+        cv::cvtColor(frame, grey, COLOR_BGR2GRAY);
+        threshold(blur_difference(grey, 7, 7, 17, 13), thr, 1, 255, THRESH_BINARY);
+        findContours(thr, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+        //cv::imshow("img", img);
+        std::cout << contours.size() << std::endl;
+
+
+        if (waitKey(5) >= 0)
+            break;
+    }
+    cap.release();
+
+    
+   
+    
+    /*
     std::string img_path = R"(IMG20230516092711.jpg)"; //TODO: delete bard picture
     cv::Mat img = cv::imread(img_path, cv::IMREAD_COLOR);
     Mat grey,thr;
@@ -28,7 +64,7 @@ int main()
     threshold(blur_difference(grey, 7, 7, 17, 13), thr, 1, 255, THRESH_BINARY);
     findContours(thr, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
     //cv::imshow("img", img);
-    std::cout << contours.size()<< std::endl;
+    std::cout << contours.size()<< std::endl;*/
     cv::waitKey(0);
     std::cout << "Hello World!\n";
 }
