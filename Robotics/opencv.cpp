195d4b2fc6943aaed8b3a07df481cc31a533127e
Alissa de Vegt <alissa.de.vegt@student.nhlstenden.com>
2023-05-25 12:17:27 +0200

    code cleanup

--- Robotics/opencv.cpp
+++ Robotics/opencv.cpp
@@ -4,20 +4,19 @@
 void opencv::detect_object(Mat img)
 {
     RNG rng(12345);
-    resize(img, img, { 500, 500 }, 0, 0, INTER_NEAREST);
-    Mat grey, thr, adthr;
+    Mat grey, thr;
     Point2f box[4];
-
-    cvtColor(img, grey, COLOR_BGR2GRAY);
-
-    threshold(blur_difference(grey, 7, 7, 17, 13), thr, 1, 255, THRESH_BINARY);
-
     vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy,approx;
-    findContours(thr, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+    vector<Vec4i> hierarchy;
 
     int hierachy_size = 0;
     int main_box = -1;
+
+    resize(img, img, { 500, 500 }, 0, 0, INTER_NEAREST);
+    cvtColor(img, grey, COLOR_BGR2GRAY);
+    threshold(blur_difference(grey, 7, 7, 17, 13), thr, 1, 255, THRESH_BINARY);
+    findContours(thr, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
     Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
 
     for (size_t i = 0; i < contours.size(); i++)
@@ -33,7 +32,7 @@ void opencv::detect_object(Mat img)
                 }
             }
 
-            if (a > hierachy_size) { 
+            if (a > hierachy_size) { //choose object with highest amounts of kids
                 hierachy_size = a;
                 main_box = i;
             }
@@ -45,14 +44,17 @@ void opencv::detect_object(Mat img)
         return;
     }
 
+    //draw rotated box that fits description
     RotatedRect rotated_rect = minAreaRect(contours[main_box]);
     rotated_rect.points(box);
-    for (int i = 0; i < 4; i++) {
+    int rotatedarea = rotated_rect.size.width * rotated_rect.size.height;
+    for (int i = 0; i < 4; i++) { //because box cant be drawen using rectangle get evry point and draw lines between each point
         line(img, box[i], box[(i + 1) % 4], color);
     }
 
-    int rotatedarea = rotated_rect.size.width * rotated_rect.size.height;
+    //center
     circle(img, rotated_rect.center, 2, color, FILLED, LINE_8);
+
 
     cout << rotated_rect.center << endl;
     cout << rotated_rect.angle << endl;
@@ -63,7 +65,7 @@ void opencv::detect_object(Mat img)
     cout << "move to x : " << 250 - rotated_rect.center.x << " and y : " << rotated_rect.center.y - 250 << endl; //use to determine how much the arm needs to rotate?
    
     imshow("img", img);
-    //imshow("cropped", cropped_image);
+
 }
 
 Mat opencv::blur_difference(Mat img, int h1, int s1, int h2, int s2)
