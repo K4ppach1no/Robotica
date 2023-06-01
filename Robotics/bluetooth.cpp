ff4c057a0352e5f09392a23eb138e0d169ad01f5
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-01 11:58:50 +0200

    example code for bluez communication over dbus

--- Robotics/bluetooth.cpp
+++ Robotics/bluetooth.cpp
@@ -2,5 +2,10 @@
 
 void bluetooth::connect()
 {
-	auto connection = sdbus::createSessionBusConnection();
+	const char* destination_name = "org.bluez.Adapter1";
+	const char* object_path = "org/bluez/hci0";
+	auto sdbus_proxy = sdbus::createProxy(destination_name, object_path);
+	sdbus_proxy->finishRegistration();
+
+	
 }
