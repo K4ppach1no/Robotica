07e22b0ac2b61f846d1dbc014b802dd3d6400920
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-01 14:27:05 +0200

    removed a single character

--- Robotics/bluetooth.cpp
+++ Robotics/bluetooth.cpp
@@ -4,7 +4,7 @@ void bluetooth::connect()
 {
 
 
-	const char* destination_name = "org.bluez.Adapter1";
+	const char* destination_name = "org.bluez.Adapter";
 	const char* object_path = "/org/bluez/hci0";
 	auto sdbus_proxy = sdbus::createProxy(destination_name, object_path);
 	sdbus_proxy->finishRegistration();
