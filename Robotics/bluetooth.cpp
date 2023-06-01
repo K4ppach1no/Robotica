f54e3a0a8659f132e8a3ddcccf0ccd102ec24799
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-01 14:22:29 +0200

    dbus bluez method call test

--- Robotics/bluetooth.cpp
+++ Robotics/bluetooth.cpp
@@ -2,10 +2,14 @@
 
 void bluetooth::connect()
 {
+
+
 	const char* destination_name = "org.bluez.Adapter1";
 	const char* object_path = "/org/bluez/hci0";
 	auto sdbus_proxy = sdbus::createProxy(destination_name, object_path);
 	sdbus_proxy->finishRegistration();
 
-	
+
+	auto method = sdbus_proxy->createMethodCall("org.bluez.Adapter1", "StartDiscovery");
+	sdbus_proxy->callMethod(method);
 }
