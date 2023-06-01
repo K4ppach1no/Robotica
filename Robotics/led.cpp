8fbbdcc0284a993402f3bf6a80ed2196cd375087
Alissa de Vegt <alissa.de.vegt@student.nhlstenden.com>
2023-06-01 09:26:05 +0200

    wat comments hoe dingen geschreven moeten worden in deze library en testen of librarie kleurtjes in de schrijv programma doet.

--- Robotics/led.cpp
+++ Robotics/led.cpp
@@ -2,6 +2,25 @@
 
 #include "led.h"
 
+#include <pigpio.h>
+#include <Windows.h> //for windows sleep 
+
+#define LED_RED 18	// Using GPIO18 for leds
+
+
 void led::aan()
 {
+	printf("Running pigpio program\n");
+	gpioInitialise(); // you MUST initialize the library !
+
+	//gpiowrite >> digital write
+	//gpioPWM >> analog write
+
+ 		/* for the pins
+		
+
+You set the mode (normally PI_INPUT or PI_OUTPUT) with gpioSetMode.
+
+You set the level (PI_LOW or PI_HIGH) with gpioWrite.
+*/
 }
