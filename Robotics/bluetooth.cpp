b58ac04540f4613c1eb805803ebbb4aaa3efeec5
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-01 12:37:55 +0200

    literally just a missing slash

--- Robotics/bluetooth.cpp
+++ Robotics/bluetooth.cpp
@@ -3,7 +3,7 @@
 void bluetooth::connect()
 {
 	const char* destination_name = "org.bluez.Adapter1";
-	const char* object_path = "org/bluez/hci0";
+	const char* object_path = "/org/bluez/hci0";
 	auto sdbus_proxy = sdbus::createProxy(destination_name, object_path);
 	sdbus_proxy->finishRegistration();
 
